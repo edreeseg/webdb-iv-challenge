4bb53a81a6a2a6b528e2031291dc5a6cdcc0986b
edreeseg <edward.reeseg@gmail.com>
2019-03-21 23:24:36 -0400

    Completing work on shopping list method, beginning to add CRUD functionality for ingredients

--- data/dataAccess.js
+++ data/dataAccess.js
@@ -3,61 +3,84 @@ const knexConfig = require('../knexfile');
 const db = knex(knexConfig.development);
 
 module.exports = {
-    getDishes,
-    addDish,
-    getDish,
-    getRecipes,
-    addRecipe,
-    getShoppingList,
+  getDishes,
+  addDish,
+  getDish,
+  getRecipes,
+  addRecipe,
+  getShoppingList,
 };
 
 function getDishes() {
-    return db('dishes');
+  return db('dishes');
 }
 
-function addDish(dish){
-    return db('dishes')
-        .insert(dish)
-        .then(res => res[0])
-        .catch(error => error);
-};
+function addDish(dish) {
+  return db('dishes')
+    .insert(dish)
+    .then(res => res[0])
+    .catch(error => error);
+}
 
-function getDish(id){
-    return new Promise(async (resolve, reject) => {
-        try {
-            const dish = (await db('dishes').where({ id }))[0]; // There must be a cleaner way to do this
-            if (!dish) reject(404); 
-            const recipes = await db('recipes').where({ dish_id: id });
-            resolve({...dish, recipes});
-        } catch {
-            reject(500);
-        }
-    });
-};
+function getDish(id) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const dish = (await db('dishes').where({ id }))[0]; // There must be a cleaner way to do this
+      if (!dish) reject(404);
+      const recipes = await db('recipes').where({ dish_id: id });
+      resolve({ ...dish, recipes });
+    } catch (e) {
+      reject(500);
+    }
+  });
+}
 
-function getRecipes(){ 
-    return new Promise(async (resolve, reject) => {
-        try {
-            const recipes = await db('recipes');
-            const dishes = await db('dishes');
-            resolve(
-                recipes.map(entry => {
-                    return {...entry, dish: dishes.find(dish => dish.id === entry.dish_id)};
-                })
-            );
-        } catch {
-            reject(500);
-        }
-    });
-};
+function getRecipes() {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const recipes = await db('recipes');
+      const dishes = await db('dishes');
+      resolve(
+        recipes.map(entry => {
+          return {
+            ...entry,
+            dish: dishes.find(dish => dish.id === entry.dish_id),
+          };
+        })
+      );
+    } catch (e) {
+      reject(500);
+    }
+  });
+}
 
-function addRecipe(recipe){ // Need to ensure recipe already exists.
-    return db('recipes')
-        .insert(recipe)
-        .then(res => res[0])
-        .catch(error => error);
-};
+function addRecipe(recipe) {
+  // Need to ensure recipe already exists.
+  return db('recipes')
+    .insert(recipe)
+    .then(res => res[0])
+    .catch(error => error);
+}
 
-function getShoppingList(id){
-    return db('ingredients');
+function getShoppingList(id) {
+  return db
+    .select('name', 'quantity', 'unit')
+    .from('recipesingredients')
+    .where({ recipe_id: id })
+    .innerJoin('ingredients', 'ingredient_id', 'ingredients.id');
+}
+
+function getIngredients() {
+  return db('ingredients');
+}
+
+function getIngredient(id) {
+  return db('ingredients').where({ id });
+}
+
+function addIngredient(ing) {
+  return db('ingredients')
+    .insert(ing)
+    .then(res => res[0])
+    .catch(err => err);
 }
--- index.js
+++ index.js
@@ -7,15 +7,21 @@ server.use(cors());
 const port = process.env.PORT || 5000;
 
 server.get('/', (req, res) => {
-    db.getShoppingList(1)
-        .then(recipes => {
-            if (recipes.errno === 19) return Promise.reject(404)
-            else res.send('test');
-        })
-        .catch(error => {
-            if (error === 404) return res.status(404).json({ error: 'Provided dish_id key must match a currently existing dish.' });
-            else return res.status(500).json({ error });
+  db.getShoppingList(3)
+    .then(recipes => {
+      if (recipes.errno === 19) return Promise.reject(404);
+      else {
+        console.log(recipes);
+        res.send('test');
+      }
+    })
+    .catch(error => {
+      if (error === 404)
+        return res.status(404).json({
+          error: 'Provided dish_id key must match a currently existing dish.',
         });
-})
+      else return res.status(500).json({ error });
+    });
+});
 
-server.listen(port, () => console.log(`Server listening on port ${port}.`))
+server.listen(port, () => console.log(`Server listening on port ${port}.`));
